1925df52d916d55e45eb69766b8fd3d2200f5c93
Glitch (hello-webpage) <none>
2020-03-08 04:38:34 +0000

    🎈🚏 Checkpoint ./script.js:12659902/445 ./index.html:12659902/124

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var sushibeans = 0;
+var sbps = 2;
 
 function morebeans() {
     sushibeans += 1;
     document.getElementById("sushibeans").innerHTML = sushibeans;
 };
+
+
+
+
+
+
+
+
+var el = document.getElementById('seconds-counter');
+
+function incrementSeconds() {
+    sushibeans += sbps;
+    document.getElementById("sushibeans").innerHTML = sushibeans;
+}
+
+var cancel = setInterval(incrementSeconds, 1000);
