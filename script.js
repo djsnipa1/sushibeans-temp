da3fed9721700ad6338514f3ee79f721059919ed
Glitch (hello-webpage) <none>
2020-03-08 04:16:33 +0000

    🚉🐦 Checkpoint ./style.css:11285567/168 ./index.html:11285567/24:12659902/473 ./script.js:12659902/229

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var sushibeans = 0
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function click() {
+  sushibeans += 1;
+}
