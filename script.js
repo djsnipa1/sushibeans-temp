a8c06caa2b747a4dc9682556a64391461f5b0938
Glitch (sushibeans) <none>
2020-03-09 20:54:56 +0000

    🎽🍠 Checkpoint ./script.js:12659902/604 ./index.html:12659902/688 ./style.css:12659902/581

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var sushibeans = 0;
-var sbps = 0;
-var sbps_cost = 10;
-var sbpsps = 0;
-var sbpsps_cost = 500;
-var potential_prestige = 0;
-var prestiges = 3000;
+var sushibeans = 0
+var sbps = 0
+var sbps_cost = 10
+var sbpsps = 0
+var sbpsps_cost = 500
+var potential_prestige = 0
+var prestiges = 0
+var multiplier = 1
+var singularities = 0
 
 var x = document.getElementById("prestigediv")
 x.style.display = "none"
@@ -16,57 +18,59 @@ var z = document.getElementById("infinity")
 z.style.display = "none"
 
 function morebeans() {
-    sushibeans += Math.round(Math.pow(1.2, prestiges));
-};
+    sushibeans += multiplier;
+}
 
 function moresbps() {
     if (sushibeans >= sbps_cost) {
-        sushibeans -= sbps_cost;
-        sbps += Math.round(Math.pow(1.2, prestiges));
-        sbps_cost = Math.round(sbps_cost * 1.1);
+        sushibeans -= sbps_cost
+        sbps += multiplier
+        sbps_cost = Math.round(sbps_cost * 1.1)
     };
-};
+}
 
 
 function moresbpsps() {
     if (sushibeans >= sbpsps_cost) {
-        sushibeans -= sbpsps_cost;
-        sbpsps += Math.round(Math.pow(1.2, prestiges));
-        sbpsps_cost = Math.round(sbpsps_cost * 1.2);
+        sushibeans -= sbpsps_cost
+        sbpsps += multiplier
+        sbpsps_cost = Math.round(sbpsps_cost * 1.2)
     };
-};
+}
 
 function prestige() {
-  if (Math.round(Math.log2(sushibeans / 1000 + 1)) > 0) {
-    prestiges += Math.round(Math.log2(sushibeans / 1000 + 1));
-    sushibeans = 0;
-    sbps = 0;
-    sbps_cost = 10;
-    sbpsps = 0;
-    sbpsps_cost = 500;
+  if (potential_prestige > 0) {
+    prestiges += Math.round(Math.log2(sushibeans / 1000 + 1))
+    sushibeans = 0
+    sbps = 0
+    sbps_cost = 10
+    sbpsps = 0
+    sbpsps_cost = 500
   }
 }
 
 function collapse() {
-  prestiges = 0
-  sushibeans = 0;
-  sbps = 0;
-  sbps_cost = 10;
-  sbpsps = 0;
-  sbpsps_cost = 500;
+  prestiges = 
+  sushibeans = 0
+  sbps = 0
+  sbps_cost = 10
+  sbpsps = 0
+  sbpsps_cost = 500
   y.style.display = "block"
   z.style.display = "none"
 }
 
 function update() {
+  multiplier = Math.round(Math.pow(1.2, prestiges))
+  potential_prestige = Math.round(Math.log2(sushibeans / 1000 + 1))
   document.getElementById("sushibeans").innerHTML = sushibeans;
   document.getElementById("sbps").innerHTML = sbps;
   document.getElementById("sbps_cost").innerHTML = sbps_cost;
   document.getElementById("sbpsps").innerHTML = sbpsps;
   document.getElementById("sbpsps_cost").innerHTML = sbpsps_cost;
   document.getElementById("prestiges").innerHTML = prestiges;
-  document.getElementById("potential_prestige").innerHTML = Math.round(Math.log2(sushibeans / 1000 + 1));
-  document.getElementById("multiplier").innerHTML = Math.round(Math.pow(1.2, prestiges));
+  document.getElementById("potential_prestige").innerHTML = potential_prestige;
+  document.getElementById("multiplier").multiplier;
 }
 
 function incrementSeconds() {
@@ -80,7 +84,7 @@ function incrementSeconds2() {
 }
 
 function prestigecheck() {
-    if (Math.round(Math.log2(sushibeans / 1000 + 1)) > 0) {
+    if (potential_prestige > 0) {
         x.style.display = "block"
     }
 }
@@ -95,20 +99,20 @@ function infinitycheck() {
 
 function hardreset() {
     if (confirm("Are you sure you want to hard reset? You will lose everything!")) {
-        sushibeans = 0;
-        sbps = 0;
-        sbps_cost = 10;
-        sbpsps = 0;
-        sbpsps_cost = 500;
-        prestiges = 0;
+        sushibeans = 0
+        sbps = 0
+        sbps_cost = 10
+        sbpsps = 0
+        sbpsps_cost = 500
+        prestiges = 0
     }
     else {
         alert("Thank goodness.")
     }
 }
 
-var cancel = setInterval(incrementSeconds, 1000);
-var cancel = setInterval(incrementSeconds2, 1000);
-var cancel = setInterval(prestigecheck, 100);
-var cancel = setInterval(infinitycheck, 10);
-var cancel = setInterval(update, 10);
+var cancel = setInterval(incrementSeconds, 1000)
+var cancel = setInterval(incrementSeconds2, 1000)
+var cancel = setInterval(prestigecheck, 100)
+var cancel = setInterval(infinitycheck, 10)
+var cancel = setInterval(update, 10)
