1811724171dc0428d9370cd9da5c7e84f58ddb48
Glitch (sushibeans) <none>
2020-03-14 04:17:41 +0000

    🎮🙈 Checkpoint ./index.html:12659902/348 ./.glitch-assets:12659902/43 ./script.js:12659902/398 ./style.css:12659902/125

--- script.js
+++ script.js
@@ -255,6 +255,21 @@ function collapse_autoclick() {
   }
 }
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+
+function setRandomColor() {
+  document.body.style.backgroundColor = ("background-color", getRandomColor())
+  $("#maingame").css("background-color", getRandomColor())
+}
 
 
 
